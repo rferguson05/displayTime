020d8a8c87b934b586d8bb696129b024fdeec0bd
rferguson05 <73495941+rferguson05@users.noreply.github.com>
2022-09-14 22:38:53 +0100

    Changed from using an api to using javascript built in time function

--- displayTime.js
+++ displayTime.js
@@ -1,10 +1,30 @@
-require("isomorphic-fetch");
-
-async function fetchTime() {
-        setInterval(async function () {
-                    const call = await fetch("http://worldtimeapi.org/api/timezone/Europe/London");
-                    const response = await call.json();
-                }, 1000);
-            }
-
-            fetchTime();
+$(document).ready(() =>{
+async function displayTime() {
+    //use setInterval to run the function every 1000ms (1s)
+    setInterval(() => {
+        //Use date constructor to create  a new object. Source https://www.w3schools.com/jsref/jsref_obj_date.asp
+        let date = new Date();
+        // create variables for the required hours, minutes seconds and AM PM
+        let hour = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        //if the hour is less than then add a 0 to pad and maintain 24 hour clock appearance
+        if (hour < 10) {
+            hour = "0" + hour;
+        }
+        //if the minutes is less than then add a 0 to pad and maintain 24 hour clock appearance
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        //if the seconds is less than then add a 0 to pad and maintain 24 hour clock appearance
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        //create variable currentTime and concatenate above variables along with the : between the numbers
+        let currentTime = hour + ":" + minutes + ":" + seconds;
+        $("#timeDiv").html(currentTime);
+       
+    }, 1000);
+}
+displayTime();
+});
